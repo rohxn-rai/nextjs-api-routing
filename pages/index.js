67140bf83cabf1c4b1ff55c55199f0b19152dd5a
rohxn-rai <rohan.rai932@outlook.com>
2025-06-23 11:08:49 -0400

    added a basic form and a post request

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,52 @@
+import { useRef } from "react";
+
 function HomePage() {
+  const emailInputRef = useRef();
+  const feedbackInputRef = useRef();
+
+  const submitFormHandler = async (e) => {
+    e.preventDefault();
+
+    const email = emailInputRef.current.value;
+    const feedback = feedbackInputRef.current.value;
+
+    const reqBody = { email, feedback };
+
+    fetch("/api/feedback", {
+      method: "POST",
+      body: JSON.stringify(reqBody),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => console.log(data));
+  };
+
   return (
     <div>
       <h1>The Home Page</h1>
+      <form onSubmit={submitFormHandler}>
+        <div>
+          <label htmlFor="email">Enter Your Email Address</label>
+          <input
+            type="email"
+            id="email"
+            placeholder="email@email.com"
+            ref={emailInputRef}
+          />
+        </div>
+        <div>
+          <label htmlFor="feedback">Enter Your Feedback</label>
+          <textarea
+            rows={5}
+            id="feedback"
+            placeholder="feedback ..."
+            ref={feedbackInputRef}
+          />
+        </div>
+        <button>Send Feedback</button>
+      </form>
     </div>
   );
 }
