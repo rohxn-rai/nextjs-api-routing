83337793741d051c70e6188d18f945341fd5de65
rohxn-rai <rohan.rai932@outlook.com>
2025-06-27 11:01:30 -0400

    created feedback route

--- pages/api/feedback.js
+++ pages/api/feedback.js
@@ -1,6 +1,15 @@
 import fs from "fs";
 import path from "path";
 
+const buildFeedbackPath = () =>
+  path.join(process.cwd(), "data", "feedback.json");
+
+const extractFeedback = (filePath) => {
+  const fileData = fs.readFileSync(filePath);
+  const data = JSON.parse(fileData);
+  return data;
+};
+
 const handler = (req, res) => {
   if (req.method === "POST") {
     const { email, feedback } = req.body;
@@ -11,16 +20,17 @@ const handler = (req, res) => {
       feedback,
     };
 
-    const filePath = path.join(process.cwd(), "data", "feedback.json");
-    const fileData = fs.readFileSync(filePath);
-    const data = JSON.parse(fileData);
+    const filePath = buildFeedbackPath();
+    const data = extractFeedback(filePath);
     data.push(newFeedback);
     fs.writeFileSync(filePath, JSON.stringify(data));
 
     res.status(201).json({ message: "Success!", feedback: newFeedback });
   } else {
+    const filePath = buildFeedbackPath();
+    const data = extractFeedback(filePath);
     res.status(200).json({
-      message: "This works!",
+      feedback: data,
     });
   }
 };
